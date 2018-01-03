6384005fe9fa615a913abab7c124a4604c6e6acb
luism <luism@Alienware>
2018-01-03 10:48:15 +0100

    miau!

--- mof/src/main/java/mof/mof/Restaurante.java
+++ mof/src/main/java/mof/mof/Restaurante.java
@@ -1,5 +1,6 @@
 package mof.mof;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -21,35 +22,25 @@ public class Restaurante {
 	
 	
 	//Metodo Modificar Restaurante
-	public void modificar(){
+	public void modificar() throws SQLException{
 		
-		/*Conexion.conectar();
+	/*	ResultSet nombre = Conexion.getSt().executeQuery("Select nombre from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet fijo = Conexion.getSt().executeQuery("Select fijo from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet movil = Conexion.getSt().executeQuery("Select movil from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet email = Conexion.getSt().executeQuery("Select email from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet descripcion = Conexion.getSt().executeQuery("Select descripcion from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet propietario = Conexion.getSt().executeQuery("Select sexo from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet comida = Conexion.getSt().executeQuery("Select password from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet url = Conexion.getSt().executeQuery("Select fechaNac from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet localizacion = Conexion.getSt().executeQuery("Select comAuton from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet precMedio = Conexion.getSt().executeQuery("Select provincia from usuario where nombreUsuario = '" + nombreUsuario + "'");
+		ResultSet foto = Conexion.getSt().executeQuery("Select localidad from usuario where nombreUsuario = '" + nombreUsuario + "'");
 		
-		try {
-		
-			Conexion.setRs(Conexion.getSt().executeQuery("SHOW TABLES"));
-			
-
-			
-		} catch (SQLException e){
-			
-			System.out.println("Erro al conectar con la base de datos mof");
-			
-		}
-		try {
-			
-			while (Conexion.getRs().next()) {
 				
-				System.out.println(Conexion.getRs().getString(1));
-				
-			}
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		}
-		
-		Conexion.cerrar();
-		*/
+		Conexion.getSt().execute("UPDATE SET "
+				+ "nombre= '" + nom + "', apellidos = '" + app + "', telefono = '" + tlf + "', email = '" + eml + "' , edad = '" + ed + "', sexo = '" + sex + "', password =  '" + pas + "', fechaNac =  '" + fec + "', comAuton = '" + com + "', provincia = '" + pro + "', localidad = '" + loc + "', foto = '" + img + "' "
+						+ "WHERE nombreUsuario =  '" + nombreUsuario + "'");
+						*/
 	}
 	
 	
