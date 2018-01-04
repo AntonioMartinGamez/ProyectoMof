87fbdf06e6741a5c9be00ad13ae30fbc8bdd0864
jihaneAkel <jihan.akel@gmail.com>
2018-01-04 08:41:00 +0100

    user

--- mof/src/main/java/mof/mof/Usuario.java
+++ mof/src/main/java/mof/mof/Usuario.java
@@ -35,12 +35,41 @@ public class Usuario {
 		consulta.next();
 		
 				
-		Conexion.getSt().execute("UPDATE SET nombre= '" + consulta.getString(2) + "', apellidos = '" + consulta.getString(2) + "', telefono = '" + consulta.getInt(4) + "', email = '" + consulta.getString(5) + "' , edad = '" + consulta.getInt(6) + "', sexo = '" + consulta.getString(7) + "', password =  '" + consulta.getString(8) + "', fechaNac =  '" + consulta.getDate(9) + "', comAuton = '" + consulta.getString(10) + "', provincia = '" + consulta.getString(11) + "', localidad = '" + consulta.getString(12) + "', foto = '" + consulta.getString(13) + "' WHERE nombreUsuario =  '" + nombreUsuario + "'");
+		Conexion.getSt().execute("UPDATE SET nombre= '" + consulta.getString(2) + "', apellidos = '" + consulta.getString(3) + "', telefono = '" + consulta.getInt(4) + "', email = '" + consulta.getString(5) + "' , edad = '" + consulta.getInt(6) + "', sexo = '" + consulta.getString(7) + "', password =  '" + consulta.getString(8) + "', fechaNac =  '" + consulta.getDate(9) + "', comAuton = '" + consulta.getString(10) + "', provincia = '" + consulta.getString(11) + "', localidad = '" + consulta.getString(12) + "', foto = '" + consulta.getString(13) + "' WHERE nombreUsuario =  '" + nombreUsuario + "'");
 		
 		Conexion.cerrar();
 		
 	}
 	
+	
+	public Usuario() {
+	
+	}
+
+	public Usuario(String nombreUsuario, String nombre, String apellidos, String email, String password,
+			String comAuton, String provincia, String localidad, String foto, String sexo, int telefono, int edad,
+			LocalDate fechaNac) {
+	
+		this.nombreUsuario = nombreUsuario;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.password = password;
+		this.comAuton = comAuton;
+		this.provincia = provincia;
+		this.localidad = localidad;
+		this.foto = foto;
+		this.sexo = sexo;
+		this.telefono = telefono;
+		this.edad = edad;
+		this.fechaNac = fechaNac;
+	}
+
+
+
+
+
+
 	public void borrar() throws SQLException{
 		Conexion.conectar();
 		
