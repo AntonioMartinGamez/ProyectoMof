04d6bfce797f648f13a89ddc9cf7072024c93a9c
luism <luism@Alienware>
2018-01-04 09:40:33 +0100

    niaaaaaaa

--- mof/src/main/java/mof/mof/Restaurante.java
+++ mof/src/main/java/mof/mof/Restaurante.java
@@ -10,6 +10,8 @@ public class Restaurante {
 	private String idRest, nombre, email, descripcion, propietario, comida, url, localizacion, foto;
 	private int fijo, movil, precMedio;
 	
+	ResultSet consulta;
+	
 	
 	//Metodo Registrar Restaurante
 	public void registro() throws SQLException{
@@ -29,22 +31,15 @@ public class Restaurante {
 		
 		Conexion.conectar();
 		
-		ResultSet nombre = Conexion.getSt().executeQuery("Select nombre from restaurante where idRest = '" + idRest + "'");
-		ResultSet fijo = Conexion.getSt().executeQuery("Select fijo from restaurante where idRest = '" + idRest + "'");
-		ResultSet movil = Conexion.getSt().executeQuery("Select movil from restaurante where idRest = '" + idRest + "'");
-		ResultSet email = Conexion.getSt().executeQuery("Select email from restaurante where idRest = '" + idRest + "'");
-		ResultSet descripcion = Conexion.getSt().executeQuery("Select descripcion from restaurante where idRest = '" + idRest + "'");
-		ResultSet propietario = Conexion.getSt().executeQuery("Select propietario from restaurante where idRest = '" + idRest + "'");
-		ResultSet comida = Conexion.getSt().executeQuery("Select comida from restaurante where idRest = '" + idRest + "'");
-		ResultSet url = Conexion.getSt().executeQuery("Select url from restaurante where idRest = '" + idRest + "'");
-		ResultSet localizacion = Conexion.getSt().executeQuery("Select localizacion from restaurante where idRest = '" + idRest + "'");
-		ResultSet precMedio = Conexion.getSt().executeQuery("Select precMedio from restaurante where idRest = '" + idRest + "'");
-		ResultSet foto = Conexion.getSt().executeQuery("Select foto from restaurante where idRest = '" + idRest + "'");
 		
+		consulta = Conexion.getSt().executeQuery("SELECT nombre, fijo, movil, email, descripcion, propietario, comida, url, localizacion, precMedio, foto FROM restaurante WHERE email = '" + email + "'");
+		
+
+		consulta.next();
 				
 		Conexion.getSt().execute("UPDATE SET "
-				+ "nombre= '" + nombre + "', fijo = '" + fijo + "', movil = '" + movil + "', email = '" + email + "' , descripcion = '" + descripcion + "', propietario = '" + propietario + "', comida =  '" + comida + "', url =  '" + url + "', localizacion = '" + localizacion + "', precMedio = '" + precMedio + "', foto = '" + foto + "' "
-						+ "WHERE idRest =  '" + idRest + "'");
+				+ "nombre= '" + consulta.getString(1) + "', fijo = '" + consulta.getInt(2) + "', movil = '" + consulta.getInt(3) + "', email = '" + consulta.getString(4) + "' , descripcion = '" + consulta.getString(5) + "', propietario = '" + consulta.getString(6) + "', comida =  '" + consulta.getString(7) + "', url =  '" + consulta.getString(8) + "', localizacion = '" + consulta.getString(9) + "', precMedio = '" + consulta.getInt(10) + "', foto = '" + consulta.getString(11) + "' "
+						+ "WHERE email =  '" + email + "'");
 				
 		Conexion.cerrar();	
 	}
@@ -55,7 +50,7 @@ public class Restaurante {
 		
 		Conexion.conectar();
 		
-		Conexion.getSt().execute("DELETE * FROM restaurante WHERE idRest = '" + idRest + "'");
+		Conexion.getSt().execute("DELETE * FROM restaurante WHERE email = '" + email + "'");
 
 		Conexion.cerrar();
 		
@@ -88,7 +83,7 @@ public class Restaurante {
 		
 		Conexion.getSt().execute("SELECT "
 				+ "nombre= '" + nombre + "', fijo = '" + fijo + "', movil = '" + movil + "', email = '" + email + "' , descripcion = '" + descripcion + "', propietario = '" + propietario + "', comida =  '" + comida + "', url =  '" + url + "', localizacion = '" + localizacion + "', precMedio = '" + precMedio + "', foto = '" + foto + "' "
-						+ "WHERE idRest =  '" + idRest + "'");
+						+ "WHERE email =  '" + email + "'");
 		
 
 		Conexion.cerrar();
@@ -103,4 +98,26 @@ public class Restaurante {
 		Comentario.contestar();
 		
 	}
+
+	//Constructor
+	public Restaurante(String nombre, String email, String descripcion, String propietario, String comida, String url,
+			String localizacion, String foto, int fijo, int movil, int precMedio) {
+		this.nombre = nombre;
+		this.email = email;
+		this.descripcion = descripcion;
+		this.propietario = propietario;
+		this.comida = comida;
+		this.url = url;
+		this.localizacion = localizacion;
+		this.foto = foto;
+		this.fijo = fijo;
+		this.movil = movil;
+		this.precMedio = precMedio;
+	}
+
+
+	public Restaurante() {
+		
+	}
+	
 }
